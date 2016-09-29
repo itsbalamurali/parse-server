1e0356cc25c172adeed0c5546461cafaf8b22695
Balamurali Pandranki <balamurali@live.com>
2016-09-29 18:09:01 +0530

    Fix and Move config file to the root dir

--- config/config.go
+++ config/config.go
@@ -11,12 +11,13 @@ import (
 
 const (
 	// defaultConfigName is the name of the config file when no alternative is supplied.
-	defaultConfigName = "config.toml"
+	defaultConfigName = "config.yml"
 )
 
 
 // cfgFile is the location of the config file
 var cfgFile string
+var config *viper.Viper
 
 
 func InitConfig()  {
@@ -27,13 +28,11 @@ func InitConfig()  {
 		fmt.Println(err)
 		os.Exit(-1)
 	}
-
-	viper.SetConfigName(defaultConfigName)
-	viper.AddConfigPath(cfgFile)
-	viper.AutomaticEnv()
-
+	config = viper.New()
+	config.SetConfigName(defaultConfigName)
+	config.AddConfigPath(cfgFile)
 	if _, err := os.Stat(cfgFile); err == nil {
-		if err := viper.ReadInConfig(); err != nil {
+		if err := config.ReadInConfig(); err != nil {
 			log.Fatalln("reading config failed:", err)
 		}
 	}
@@ -44,30 +43,36 @@ func findConfig() (string, error) {
 	if cfgFile != "" {
 		return cfgFile, nil
 	}
-
-	ch := configHome()
-	if err := os.MkdirAll(ch, 0755); err != nil {
-		return "", err
-	}
-
-	return filepath.Join(ch, defaultConfigName), nil
+	return filepath.Join(configPath(), defaultConfigName), nil
 }
 
 func configPath() string {
-	return fmt.Sprintf("%s/%s", configHome(), defaultConfigName)
+	wd,err := os.Getwd()
+	if err != nil {
+		log.Fatalln("can't determine config path", err)
+	}
+	return fmt.Sprintf("%s/%s", wd, defaultConfigName)
 }
 
 
-func configHome() string {
-	configHome := os.Getenv("XDG_CONFIG_HOME")
-	if configHome == "" {
-		configHome = filepath.Join(homeDir(), ".config", "doctl")
+// Get fetches a generic value from viper config
+func Get(key string) interface{} {
+
+	// parse the config file if it's not already
+	if config == nil {
+		InitConfig()
 	}
 
-	return configHome
+	return config.Get(key)
 }
 
+// Viper returns the viper config object
+func Viper() *viper.Viper {
 
-func homeDir() string {
-	return os.Getenv("HOME")
+	// parse the config file if it's not already
+	if config == nil {
+		 InitConfig()
+	}
+
+	return config
 }
