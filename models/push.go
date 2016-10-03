4617a544fecb29065af76387637b590c405acedd
Balamurali Pandranki <balamurali@live.com>
2016-10-04 04:59:58 +0530

    Added Push Model and Funcmap

--- models/push.go
+++ models/push.go
@@ -1,1 +1,15 @@
 package models
+
+type Push struct {
+	Where	Where	`json:"where"`
+	Channels []string `json:"channels"`
+	Data	Data	`json:"data"`
+}
+
+type Where struct {
+	InstalationId	string	`json:"installationId"`
+}
+
+type Data struct {
+	Alert	string	`json:"alert"`
+}
