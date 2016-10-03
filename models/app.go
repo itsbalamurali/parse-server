976374b599e623a905f441ec28fe70db0d53300c
Balamurali Pandranki <balamurali@live.com>
2016-10-04 05:23:55 +0530

    Added App Model

--- models/app.go
+++ models/app.go
@@ -1,5 +1,17 @@
 package models
 
 type App struct {
-
+	AppName string `json:"appName"`
+	ApplicationID string `json:"applicationId"`
+	ClientClassCreationEnabled bool `json:"clientClassCreationEnabled"`
+	ClientPushEnabled bool `json:"clientPushEnabled"`
+	DashboardURL string `json:"dashboardURL"`
+	JavascriptKey string `json:"javascriptKey"`
+	MasterKey string `json:"masterKey"`
+	RequireRevocableSessions bool `json:"requireRevocableSessions"`
+	RestKey string `json:"restKey"`
+	RevokeSessionOnPasswordChange bool `json:"revokeSessionOnPasswordChange"`
+	WebhookKey string `json:"webhookKey"`
+	WindowsKey string `json:"windowsKey"`
 }
+
